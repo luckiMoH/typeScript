4813e79d25dae75b20274fbeb169184a8e4049f4
LuckiMoH <mat.maj1992@gmail.com>
2022-12-27 13:00:15 +0100

    TypeScript learning

---
+++ 2. TypeScript Basics and Basic Types/1. Using Types/app.ts
@@ -0,0 +1,14 @@
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];
+} = {
+// const person = {
+    name: 'Mateusz',
+    age: 30,
+    hobbies: ['sports', 'computer science'],
+    role: [2, 'author'],
+};
+
+console.log(person);
